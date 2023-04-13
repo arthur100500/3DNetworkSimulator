bc32cefba9b5f33b488bc55256b9d5471bbef404
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-04-13 03:20:29 +0300

    Fix compile error

--- 3D Network Simulator/Assets/Objects/Devices/PC/Laptop/Laptop.cs
+++ 3D Network Simulator/Assets/Objects/Devices/PC/Laptop/Laptop.cs
@@ -46,7 +46,7 @@ namespace Objects.Devices.PC.Laptop
 
         public override void OpenConsole()
         {
-            if (!Node.Started)
+            if (!Node.IsReady)
             {
                 GlobalNotificationManager.AddMessage("[<color=red>FAIL</color>] Can't connect to " + Node.Name +
                                                      " as it is not loaded");
