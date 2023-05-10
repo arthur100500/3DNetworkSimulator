b65417c3252f37db959e861404dc0c6a5a61fcb7
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-05-10 21:39:02 +0300

    Add auth and projects list

--- 3D Network Simulator/Assets/Menu/Auth/AuthMenu.cs
+++ 3D Network Simulator/Assets/Menu/Auth/AuthMenu.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
+using Menu.Json;
+using Menu.ProjectSelect;
+using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -14,8 +18,10 @@ namespace Menu.Auth
         [SerializeField] private TMP_InputField passwordInputField;
         [SerializeField] private Button submitButton;
         [SerializeField] private TMP_Text errors;
-        
+        [SerializeField] private ProjectSelector selector;
+
         private const string RequestUrl = "http://127.0.0.1:10203/login";
+        private const string RequestUrlList = "http://127.0.0.1:10203/ns/projects";
 
         private void Start()
         {
@@ -27,49 +33,60 @@ namespace Menu.Auth
             var loginData = loginInputField.text;
             var passwordData = passwordInputField.text;
             var loginJson = $"{{\"Username\": \"{loginData}\", \"Password\": \"{passwordData}\"}}";
-            
+
             var request = new UnityWebRequest(RequestUrl);
             request.uploadHandler = new UploadHandlerRaw(Encoding.ASCII.GetBytes(loginJson));
             request.downloadHandler = new DownloadHandlerBuffer();
             request.method = UnityWebRequest.kHttpVerbPOST;
-            
-            StartCoroutine(SendAuth(request, OnRequest));
-        }
-        
-        IEnumerator SendAuth(UnityWebRequest request, Action<UnityWebRequest> callback)
-        {
-            using (request)
-            {
-                yield return request.SendWebRequest();
 
-                callback(request);
-            }
+            StartCoroutine(SendRequest(request, OnRequest));
+        }
+
+        IEnumerator SendRequest(UnityWebRequest request, Action<UnityWebRequest> callback)
+        {
+            yield return request.SendWebRequest();
+
+            callback(request);
         }
 
         private void OnRequest(UnityWebRequest request)
         {
             var code = request.responseCode;
             var errorsArray = request.downloadHandler.text;
-            
+
             errors.text = errorsArray;
 
             if (code != 200)
                 return;
-            
+
             var headers = request.GetResponseHeaders();
-            
+
             if (headers is null || !headers.ContainsKey("Set-Cookie"))
                 return;
-            
+
             var authCookie = headers["Set-Cookie"];
-            
+
             Authorize(authCookie);
         }
 
         private void Authorize(string cookie)
         {
-            
+            var request = new UnityWebRequest(RequestUrlList);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.method = UnityWebRequest.kHttpVerbGET;
+            request.SetRequestHeader("Cookie", cookie);
+
+            StartCoroutine(SendRequest(request, InitSelector));
+        }
+
+        private void InitSelector(UnityWebRequest request)
+        {
+            var NsJProjectListJson = request.downloadHandler.text;
+            var NsJProjectList = JsonConvert.DeserializeObject<List<NsJProject>>(NsJProjectListJson);
+
+            selector.Init(NsJProjectList);
+
+            gameObject.SetActive(false);
         }
     }
 }
-            
---
+++ 3D Network Simulator/Assets/Menu/Json/NsJProject.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace Menu.Json
+{
+    [Preserve]
+    public class NsJProject
+    {
+        [Preserve]
+        public int Id { get; set; }
+        [Preserve]
+        public string Name { get; set; }
+        [Preserve]
+        public string GnsId { get; set; }
+        [Preserve]
+        public string OwnerId { get; set; }
+        [Preserve]
+        public string JsonAnnotation { get; set; }
+        
+        [JsonConstructor] 
+        public NsJProject() { }
+    }
+}
